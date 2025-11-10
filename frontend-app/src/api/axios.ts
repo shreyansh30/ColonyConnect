9387f979a300d272fe0279f7d6e4ba49949a45f0
shreyansh30 <83909772+shreyansh30@users.noreply.github.com>
2025-11-10 17:57:09 +0530

    ColonyConnect v1.5

--- frontend-app/src/api/axios.ts
+++ frontend-app/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://colonyconnect.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
